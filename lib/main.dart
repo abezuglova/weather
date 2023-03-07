e4a25ceb749cb72a053817aecf760343021ee84a
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-03-07 13:13:50 +0300

    Location repository provided

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,38 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/i_repository/i_location_repository.dart';
 import 'package:weather_app/i_repository/i_weather_report_repository.dart';
 import 'package:weather_app/pages/forecast_page/forecast_page.dart';
+import 'package:weather_app/repository/location_repository.dart';
 import 'package:weather_app/repository/weather_report_repository.dart';
 
 void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final dio =
+      Dio(BaseOptions(baseUrl: 'https://weatherapi-com.p.rapidapi.com'));
+      
   @override
   Widget build(BuildContext context) {
-    return RepositoryProvider<IWeatherReportRepository> (
-      create: (context) => WeatherReportRepository(
-        Dio(
-          BaseOptions(baseUrl: 'https://weatherapi-com.p.rapidapi.com'),
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<IWeatherReportRepository>(
+          create: (context) => WeatherReportRepository(dio),
         ),
-      ),
+        RepositoryProvider<ILocationRepository>(
+          create: (context) => LocationRepository(dio),
+        ),
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
